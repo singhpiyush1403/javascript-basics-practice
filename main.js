8397b5daea1e8b44c7edb835ae84f7748a72d498
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-06-23 16:48:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+
+
+// console.log("Piyush kumar")
+
+// let name = "Piyush"
+// console.log(name)
+
+
+// let age = 22
+// console.log(age)
+
+// let name = "kumar"
+// console.log(name.length)
+
+// let marks = [10,20,30,40,50,"false", "piyush"]
+// console.log(marks)
+// console.log(marks[0])
+// console.log(marks[1])
+// console.log(marks[2])
+// console.log(marks[3])
+// console.log(marks.length)
+
+// Arithmetic operator
+let a = 10
+let b = 20
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+// assignment operator
+
+a = a+5
+a = a*5
+console.log(a)
+console.log(a)
+
+// comparison operator
+ 
+let a1 = 10
+let a2 = 15
+console.log(a1==a1)
+console.log(a1 !=a2)
+console.log(a1===a2)
+console.log(a1>a2)
+console.log(a1<a2)
+console.log(a1>=a2)
+console.log(a1<=a2)
+
+
+
+// for (let i = 1; i<=5; i++){
+//   if(i==3){
+//     break;
+//   }
+//   console.log(i)
+// }
+
+// for(let i=1; i<=5; i++){
+//   if(i==3){
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+
+let age = 20
+if(age>=18){
+  console.log("Adult")
+}
+else{
+  console.log("Not Adult")
+}
+
+
+
+let marks = 55
+if(marks>=75){
+  console.log("A")
+}
+else if(marks>=65){
+  console.log("B")
+}
+else if(marks>=55){
+  console.log("C")
+}
+else{
+  console.log("Out of Range")
+}
+  
+
+// for(let i = 1; i<=5; i++){
+//   console.log("Number is :",i)
+// }
+
+// let i = 1
+// while(i<=5){
+//   console.log("number is",i)
+// i++
+// }
+
+
+let i = 1
+do{
+  console.log(i)
+  i++
+}
+while(i<=5)
+
+
+
+
+
